026a72a2695711b5d8a3cd3126adec2d2fadc528
teddysmithdev <teddysmithdev@gmail.com>
2024-07-30 10:28:54 -0400

    Maximum Subarray

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,28 +1,15 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 public class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        if(root == null) return result;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int levelSize = 0;
-        while(!queue.isEmpty()) {
-            List<Integer> levelNodes = new ArrayList<>();
-            levelSize = queue.size();
-            for(int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.remove();
-                levelNodes.add(node.val);
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
-            result.add(levelNodes);
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0;
+        for (int num : nums) {
+            //This is the negative number reset
+            currentSum = Math.max(currentSum, 0);
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
         }
-        return result;
+        return maxSum;
     }
 }
