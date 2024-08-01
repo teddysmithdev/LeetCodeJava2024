cd722864ac7f9090c3023dcc9865139cbce62e27
teddysmithdev <teddysmithdev@gmail.com>
2024-08-01 09:39:24 -0400

    Roman To Integer

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,15 +1,30 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int currentSum = 0;
-        for (int num : nums) {
-            //This is the negative number reset
-            currentSum = Math.max(currentSum, 0);
-            currentSum += num;
-            maxSum = Math.max(maxSum, currentSum);
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+        for(char c : s.toCharArray()) {
+            int currentValue = map.get(c);
+            if(currentValue > prevValue) {
+                total += currentValue - (2 * prevValue);
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
         }
-        return maxSum;
+        return total;
     }
 }
