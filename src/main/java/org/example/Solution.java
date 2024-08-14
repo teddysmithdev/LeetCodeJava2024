6219281bfce8f52b0c4926cc4d3fb3ba7255908d
teddysmithdev <teddysmithdev@gmail.com>
2024-08-14 09:20:07 -0400

    Merge Intervals

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,39 +1,26 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Solution {
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if(digits.isEmpty()) {
-            return result;
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][0];
         }
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        backtrack(digits, 0, "", result, map);
-        return result;
-    }
-
-
-    private void backtrack(String digits, int index, String current, List<String> result, Map<Character, String> dic) {
-        if (index == digits.length()) {
-            result.add(current);
-            return;
+        for(int i = 0; i < intervals.length; i++) {
+            int tempMin = intervals[i][0];
+            int tempMax = intervals[i][1];
+            while(i + 1 < intervals.length && (intervals[i + 1][0] <= tempMax || intervals[i + 1][1] <= tempMax)) {
+                tempMax = Math.max(tempMax, intervals[i + 1][1]);
+                i++;
+            }
+            ans.add(new int[]{tempMin, tempMax});
         }
-        String letters = dic.get(digits.charAt(index));
-        for(char letter : letters.toCharArray()) {
-            backtrack(digits, index + 1, current + letter, result, dic);
-        }
+        return ans.toArray(new int[0][]);
     }
 }
