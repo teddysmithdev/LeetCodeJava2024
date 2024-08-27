3abf1f6e5b5ce8af0005cc4685ff8004f0813b41
teddysmithdev <teddysmithdev@gmail.com>
2024-08-27 07:53:35 -0400

    Best Time To Buy And Sell Stock

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,20 +1,14 @@
 package org.example;
 
 public class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode preHead = new ListNode(0);
-        ListNode last = preHead;
-        while(list1 != null && list2 != null) {
-            if(list1.val > list2.val) {
-                last.next = list2;
-                list2 = list2.next;
-            } else {
-                last.next = list1;
-                list1 = list1.next;
-            }
-            last = last.next;
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 0) return 0;
+        int minPrice = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            profit = Math.max(profit, prices[i] - minPrice);
         }
-        last.next = list1 == null ? list2 : list1;
-        return preHead.next;
+        return profit;
     }
 }
