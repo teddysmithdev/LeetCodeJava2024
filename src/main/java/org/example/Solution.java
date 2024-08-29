16f3823b255b97c41e8e1fc2c9d51dd057011343
teddysmithdev <teddysmithdev@gmail.com>
2024-08-29 14:10:03 -0400

    Jump Game

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,24 +1,12 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> r = new ArrayList<>();
-        for(int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            for(int j = 0; j <= i; j++) {
-                row.add(1);
-            }
-            for(int j = 1; j < i; j++) {
-                int valueAboveLeft = r.get(i - 1).get(j - 1);
-                int valueAboveRight = r.get(i - 1).get(j);
-                int sum = valueAboveLeft + valueAboveRight;
-                row.set(j, sum);
-            }
-            r.add(row);
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(i > farthest) return false;
+            farthest = Math.max(farthest, i + nums[i]);
         }
-        return r;
+        return true;
     }
 }
