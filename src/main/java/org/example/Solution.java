1d331411f1dab7c90e8af83c651a9a62f26fae2e
teddysmithdev <teddysmithdev@gmail.com>
2024-09-02 10:55:00 -0400

    Same Tree

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
-    public boolean canJump(int[] nums) {
-        int farthest = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(i > farthest) return false;
-            farthest = Math.max(farthest, i + nums[i]);
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while (!queue.isEmpty()) {
+            p = queue.poll();
+            q = queue.poll();
+            if (p == null && q == null) continue;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+            queue.add(p.left);
+            queue.add(q.left);
+            queue.add(p.right);
+            queue.add(q.right);
         }
         return true;
     }
