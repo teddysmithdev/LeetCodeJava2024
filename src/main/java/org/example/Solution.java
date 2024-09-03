c60dfdf8fb65e7537bd3acadd823414e91bc62a6
teddysmithdev <teddysmithdev@gmail.com>
2024-09-03 15:01:10 -0400

    Diameter Of Binary Tree

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,20 +1,20 @@
 package org.example;
 
 public class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0; i < (n + 1)/2; i++) {
-            for(int j = 0; j < n/2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                //Bottom right to bottom left
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-                //Top right to bottom right
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                //Top left to top right
-                matrix[j][n - 1 - i] = matrix[i][j];
-                //Temp to top left
-                matrix[i][j] = temp;
-            }
-        }
+    private int diameter;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        longestPath(root);
+        return diameter;
+    }
+
+    private int longestPath(TreeNode root) {
+        if (root == null) return 0;
+        int leftPath = longestPath(root.left);
+        int rightPath = longestPath(root.right);
+
+        diameter = Math.max(diameter, leftPath + rightPath);
+
+        return Math.max(leftPath, rightPath) + 1;
     }
 }
