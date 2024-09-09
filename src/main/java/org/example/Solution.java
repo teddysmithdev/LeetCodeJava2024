e0ce32aa8bb303832be5ca994f2a97acccb98399
teddysmithdev <teddysmithdev@gmail.com>
2024-09-09 17:07:11 -0400

    Longest Consecutive Sequence

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,20 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        Set set = new HashSet();
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if(num != '.') {
-                    if(!set.add(num + " in row " + i) ||
-                    !set.add(num + " in column " + j) ||
-                    !set.add(num + " in block " + i/3 + "-" + j/3)) {
-                        return false;
-                    }
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int num : nums) {
+            uniqueNumbers.add(num);
+        }
+        int maxLengthOfSequence = 0;
+        for(int num : uniqueNumbers) {
+            if(!uniqueNumbers.contains(num - 1)) {
+                int currentLengthOfSequence = 1;
+
+                while(uniqueNumbers.contains(num + 1)) {
+                    num++;
+                    currentLengthOfSequence++;
                 }
+                maxLengthOfSequence = Math.max(maxLengthOfSequence, currentLengthOfSequence);
             }
         }
-        return true;
+        return maxLengthOfSequence;
     }
 }
