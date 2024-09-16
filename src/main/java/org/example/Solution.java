d63a61a0a4504ba46fd878edd5cba0abef1c7794
teddysmithdev <teddysmithdev@gmail.com>
2024-09-16 10:32:46 -0400

    Spiral Matrix

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,27 +1,22 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        permutation(nums, result, new LinkedHashSet<>());
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return result;
+        int rows = matrix.length; int cols = matrix[0].length;
+        int up = 0, down = rows - 1, left = 0, right = cols - 1;
+
+        while(up <= down && left <= right) {
+            for(int col = left; col <= right; col++) result.add(matrix[up][col]);
+            for(int row = up + 1; row <= down; row++) result.add(matrix[row][right]);
+            if(up != down) for(int col = right - 1; col >= left; col--) result.add(matrix[down][col]);
+            if(left != right) for(int row = down - 1; row > up; row--) result.add(matrix[row][left]);
+            up++; down--; left++; right--;
+        }
         return result;
-    }
-    private void permutation(int[] nums, List<List<Integer>> result, Set<Integer> set) {
-        if(set.size() == nums.length) {
-            result.add(new ArrayList<>(set));
-            return;
-        }
-        for(int i = 0; i < nums.length; i++) {
-            if(!set.contains(nums[i])) {
-                set.add(nums[i]);
-                permutation(nums, result, set);
-                set.remove(nums[i]);
-            }
-        }
     }
 }
