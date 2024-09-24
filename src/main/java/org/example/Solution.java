d6dce424b474dc2e5e3819f0c8e366ef54983140
teddysmithdev <teddysmithdev@gmail.com>
2024-09-24 11:03:56 -0400

    Linked-List Cycle

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,41 +1,19 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : nums) {
-            list.add(num);
+    public boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
         }
-        return quickSelect(list, k);
-    }
-    private int quickSelect(List<Integer> nums, int k) {
-        if(nums.size() == 1) return nums.get(0);
-        int pivotIndex = new Random().nextInt(nums.size());
-        int pivot = nums.get(pivotIndex);
-
-        List<Integer> less = new ArrayList<>();
-        List<Integer> equal = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-
-        for(int num : nums) {
-            if(num > pivot) {
-                greater.add(num);
-            } else if(num < pivot) {
-                less.add(num);
-            } else {
-                equal.add(num);
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow != fast) {
+            if(fast == null || fast.next == null) {
+                return false;
             }
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        if(k <= greater.size()) {
-            return quickSelect(greater, k);
-        }
-        if(greater.size() + equal.size() < k) {
-            return quickSelect(less, k - greater.size() - equal.size());
-        }
-        return pivot;
+        return true;
     }
 }
