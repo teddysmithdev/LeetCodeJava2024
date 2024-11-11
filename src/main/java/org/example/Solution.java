ed3c74f27394a10b4b8b21879c4033eca855b8bb
teddysmithdev <teddysmithdev@gmail.com>
2024-11-11 09:53:02 -0500

    Longest Common Prefix

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,26 +1,18 @@
 package org.example;
 
-import java.util.*;
-
 public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            Set<Integer> seen = new HashSet<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = -nums[i] - nums[j];
-                if (seen.contains(complement)) {
-                    int a = nums[i];
-                    int b = nums[j];
-                    int c = complement;
-                    List<Integer> list = Arrays.asList(Math.min(a, Math.min(b, c)),
-                            a + b + c - Math.min(a, Math.min(b,c)) - Math.max(a, Math.max(b, c)),
-                            Math.max(a, Math.max(b, c)));
-                    result.add(list);
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char currentChar = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                if(i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
+                    return strs[0].substring(0, i);
                 }
-                seen.add(nums[j]);
             }
         }
-        return new ArrayList<>(result);
+        return strs[0];
     }
 }
