1defc0a4a875d8079051a0d9bb7d97f5dab4a191
teddysmithdev <teddysmithdev@gmail.com>
2024-11-13 09:43:22 -0500

    Rotate Array

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,27 +1,20 @@
 package org.example;
 
 public class Solution {
-    public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-        while (left < right) {
-            if (height[left] <= height[right]) {
-                if(height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    water += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if(height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    water += rightMax - height[right];
-                }
-                right--;
-            }
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        return water;
     }
 }
