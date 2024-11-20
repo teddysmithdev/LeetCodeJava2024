7c0be03a3a0dfbfe771c159aec9035bf27811eef
teddysmithdev <teddysmithdev@gmail.com>
2024-11-20 12:01:04 -0500

    Maximum Product Subarray

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,30 +1,24 @@
 package org.example;
 
 public class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    public int maxProduct(int[] nums) {
+        if(nums.length == 0) return 0;
 
-            if(nums[mid] == target) {
-                return mid;
-            }
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int result = nums[0];
 
-            if(nums[left] <= nums[mid]) {
-                if(nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if(nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
+        for(int i = 1; i < nums.length; i++) {
+            int current = nums[i];
+
+            int tempMax = Math.max(current, Math.max(maxProduct * current, minProduct * current));
+
+            minProduct = Math.min(current, Math.min(maxProduct * current, minProduct * current));
+            maxProduct = tempMax;
+
+            result = Math.max(result, maxProduct);
         }
-        return -1;
+
+        return result;
     }
 }
