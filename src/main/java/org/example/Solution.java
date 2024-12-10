301ce95878f8d8f6ef9920c42efa053797177f8e
teddysmithdev <teddysmithdev@gmail.com>
2024-12-10 09:52:56 -0500

    ZigZag Conversion

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,29 +1,24 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
 public class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) return new ArrayList<Integer>();
+    public String convert(String s, int numRows) {
+        if(numRows == 1 || s.length() <= numRows) return s;
 
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        List<Integer> result = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for(int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; i++) rows[i] = new StringBuilder();
 
-                if(i == size - 1) result.add(node.val);
+        int curRow = 0;
+        boolean goingDown = false;
 
-                if(node.left != null) queue.offer(node.left);
-
-                if(node.right != null) queue.offer(node.right);
-            }
+        for(char c : s.toCharArray()) {
+            rows[curRow].append(c);
+            if(curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
         }
-        return result;
+
+        StringBuilder result = new StringBuilder();
+        for(StringBuilder row : rows) result.append(row);
+
+        return result.toString();
     }
 }
