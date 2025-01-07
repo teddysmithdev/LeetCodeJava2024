d6c8ee8089b13290f0a9248001c4e22b6fc433b5
teddysmithdev <teddysmithdev@gmail.com>
2025-01-07 11:33:31 -0500

    Word Search

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        char[][] board2 = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        };
+        String word2 = "SEE"; // Expected: true
+        solution.exist(board2, word2);
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,20 +1,29 @@
 package org.example;
 
-import java.util.Stack;
-
 public class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int index = stack.pop();
-                result[index] = i - index;
+    public boolean exist(char[][] board, String word) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(dfs(board, word, 0, i, j)){
+                    return true;
+                }
             }
-            stack.push(i);
         }
-        return result;
+        return false;
+    }
+    private boolean dfs(char[][] board, String word, int index, int i, int j){
+        if(index == word.length()) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(index)) return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found = dfs(board, word, index + 1, i + 1, j) ||
+                dfs(board, word, index + 1, i - 1, j) ||
+                dfs(board, word, index + 1, i, j + 1) ||
+                dfs(board, word, index + 1, i, j - 1);
+
+        board[i][j] = temp;
+        return found;
     }
 }
