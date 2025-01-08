bf3fa207d8c6a32662fadfb28d02790bf89893f3
teddysmithdev <teddysmithdev@gmail.com>
2025-01-08 07:07:00 -0500

    Lowest Common Ancestor of Binary Search Tree

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,29 +1,16 @@
 package org.example;
 
 public class Solution {
-    public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(dfs(board, word, 0, i, j)){
-                    return true;
-                }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null) {
+            if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
             }
         }
-        return false;
-    }
-    private boolean dfs(char[][] board, String word, int index, int i, int j){
-        if(index == word.length()) return true;
-        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(index)) return false;
-
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        boolean found = dfs(board, word, index + 1, i + 1, j) ||
-                dfs(board, word, index + 1, i - 1, j) ||
-                dfs(board, word, index + 1, i, j + 1) ||
-                dfs(board, word, index + 1, i, j - 1);
-
-        board[i][j] = temp;
-        return found;
+        return null;
     }
 }
---
+++ src/main/java/org/example/TreeNode.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x ;}
+}
